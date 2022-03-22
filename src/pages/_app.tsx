a86a204f708e707c372b43e15fa4b0427e92fdcc
MichalK98 <michal10203040@gmail.com>
2022-03-22 22:39:39 +0100

    Add styled-components and GlobalStyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import type { AppProps } from 'next/app';
+import GlobalStyle from 'src/GlobalStyle';
 
 import DefaultLayout from '../layouts/DefaultLayout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <DefaultLayout>
         <Component {...pageProps} />
       </DefaultLayout>
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
